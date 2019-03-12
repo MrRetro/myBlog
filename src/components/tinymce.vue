4f740d194fd2492a6a32337d12e7079574f3d217
Apple <>
2019-03-12 18:48:03 +0800

    tinymce接入80%

--- src/components/tinymce.vue
+++ src/components/tinymce.vue
@@ -2,10 +2,12 @@
   <textarea :id="id" :value="value"></textarea>
 </template>
 <script>
+import $ from 'jquery'
 // Import TinyMCE
 import tinymce from 'tinymce/tinymce'
 import 'tinymce/themes/modern/theme'
 import 'tinymce/plugins/image'
+import 'tinymce/plugins/imagetools'
 import 'tinymce/plugins/link'
 import 'tinymce/plugins/code'
 import 'tinymce/plugins/table'
@@ -14,9 +16,15 @@ import 'tinymce/plugins/contextmenu'
 import 'tinymce/plugins/wordcount'
 import 'tinymce/plugins/colorpicker'
 import 'tinymce/plugins/textcolor'
-const INIT = 0
-const CHANGED = 2
-// var EDITOR = null
+import 'tinymce/plugins/print'
+import 'tinymce/plugins/preview'
+import 'tinymce/plugins/media'
+import 'tinymce/plugins/fullpage'
+import 'tinymce/plugins/fullscreen'
+import 'tinymce/plugins/emoticons'
+import 'tinymce/plugins/advlist'
+import 'tinymce/plugins/autolink'
+import 'tinymce/plugins/paste'
 export default {
   props: {
     value: {
@@ -25,53 +33,127 @@ export default {
     },
     setting: {}
   },
-  watch: {
-    value: function (val) {
-      console.log('init ' + val)
-      if (this.status === INIT || tinymce.activeEditor.getContent() !== val) {
-        tinymce.activeEditor.setContent(val)
+  mounted: function () {
+    const _this = this
+    const setting =
+      {
+        selector: '#' + _this.id,
+        language_url: '/static/tinymce/zh_CN.js',
+        language: 'zh_CN',
+        skin_url: '/static/tinymce/skins/lightgray',
+        init_instance_callback: function (editor) {
+          // EDITOR = editor
+          console.log('Editor: ' + editor.id + ' is now initialized.')
+          editor.on('input change undo redo', () => {
+            var content = editor.getContent()
+            _this.$emit('input', content)
+          })
+        },
+        plugins: 'uploadimage link lists image code table colorpicker textcolor wordcount contextmenu print preview media fullpage fullscreen emoticons imagetools paste',
+        toolbar: [
+          'undo redo fontselect fontsizeselect removeformat imagetools paste uploadimage',
+          'bold italic underline strikethrough backcolor forecolor alignleft aligncenter alignright alignjustify bullist numlist outdent indent blockquote link unlink image code print preview media fullpage fullscreen emoticons'],
+        font_formats: '微软雅黑=\'微软雅黑\',microsoft yahei;Arial=\'Arial\',Helvetica,Arial,sans-serif;Times New Roman=\'Times New Roman\',Helvetica,Arial,sans-serif;宋体=\'宋体\',Helvetica,Arial,sans-serif;仿宋=\'仿宋\',Helvetica,Arial,sans-serif;仿宋GB2312=\'仿宋GB2312\',Helvetica,Arial,sans-serif;楷体=\'楷体\',Helvetica,Arial,sans-serif;黑体=\'黑体\',Helvetica,Arial,sans-serif;华文新魏=\'华文新魏\',Helvetica,Arial,sans-serif;隶书=\'隶书\',Helvetica,Arial,sans-serif',
+        automatic_uploads: true,
+        image_advtab: true,
+        table_default_styles: {
+          width: '100%',
+          borderCollapse: 'collapse'
+        },
+        image_title: false, // 是否开启图片标题设置的选择，这里设置否
+        fontsize_formats: '11px 12px 14px 16px 18px 24px 36px 48px',
+        branding: false,
+        setup (editor) {
+          editor.on('Change', function (e) {
+            // console.log(e.level.content)
+            _this.$emit('onContent', e.level.content)
+          })
+          editor.on('Redo', function (e) {
+            console.log('User has pressed redo')
+          })
+        }
       }
-      this.status = CHANGED
-    }
+    Object.assign(setting, _this.setting)
+
+    // 图片上传
+    this.addUploadImage()
+
+    tinymce.init(setting)
   },
   data: function () {
     return {
-      status: INIT,
       id: 'editor-' + new Date().getMilliseconds()
     }
   },
   methods: {
-  },
-  mounted: function () {
-    const _this = this
-    const setting =
-        {
-          selector: '#' + _this.id,
-          language: 'zh_CN',
-          skin_url: '/static/tinymce/skins/lightgray',
-          init_instance_callback: function (editor) {
-            // EDITOR = editor
-            console.log('Editor: ' + editor.id + ' is now initialized.')
-            editor.on('input change undo redo', () => {
-              var content = editor.getContent()
-              _this.$emit('input', content)
+    clear () {
+      tinymce.activeEditor.setContent('')
+    },
+    addUploadImage () {
+      tinymce.PluginManager.add('uploadimage', function (editor) {
+        function selectLocalImages () {
+          var dom = editor.dom
+          var input = $('<input type="file" id="file" name="file" accept="image/jpg,image/jpeg,image/png,image/gif" multiple="multiple">')
+          input.on('change', function () {
+            var form = $('<form id="form"/>',
+              {
+                action: editor.settings.upload_image_url, // 设置上传图片的路由，配置在初始化时
+                style: 'display:none',
+                method: 'post',
+                enctype: 'multipart/form-data'
+              }
+            )
+            form.append(input)
+            $('body').append(form)
+
+            var data = new FormData($('#form')[0])
+            $.ajax({
+              type: 'post',
+              url: editor.settings.upload_image_url,
+              data: data,
+              processData: false,
+              contentType: false,
+              success: function (res) {
+                console.log('success------------')
+                console.log(res)
+              }
             })
-          },
-          plugins: 'link lists image code table colorpicker textcolor wordcount contextmenu',
-          toolbar:
-            'bold italic underline strikethrough | fontsizeselect | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist | outdent indent blockquote | undo redo | link unlink image code | removeformat',
-          branding: false,
-          setup (editor) {
-            editor.on('change', function (e) {
-              console.log(e.level.content)
-            })
-            editor.on('Redo', function (e) {
-              console.log('User has pressed redo')
-            })
-          }
+
+            // ajax提交表单
+            // form.ajaxSubmit({
+            //   beforeSubmit: function () {
+            //     return true
+            //   },
+            //   success: function (data) {
+            //     if (data && data.file_path) {
+            //       editor.focus()
+            //       data.file_path.forEach(function (src) {
+            //         editor.selection.setContent(dom.createHTML('img', {src: src}))
+            //       })
+            //     }
+            //   }
+            // })
+          })
+
+          input.click()
         }
-    Object.assign(setting, _this.setting)
-    tinymce.init(setting)
+
+        editor.addCommand('mceUploadImageEditor', selectLocalImages)
+
+        editor.addButton('uploadimage', {
+          icon: 'image',
+          tooltip: '上传图片',
+          onclick: selectLocalImages
+        })
+
+        editor.addMenuItem('uploadimage', {
+          icon: 'image',
+          text: '上传图片',
+          context: 'tools',
+          onclick: selectLocalImages
+        })
+      })
+    }
   },
   beforeDestroy: function () {
     tinymce.get(this.id).destroy()
--- src/views/Log/form.vue
+++ src/views/Log/form.vue
@@ -1,14 +1,18 @@
 <template>
-    <div>
+    <div class="form-box">
+      <div class="title-box">
+        <p class="title">标题</p>
+        <input class="input" type="text" />
+      </div>
       <editor
         class="editor"
         ref="edit"
-        :value="content"
         :setting="editorSetting"
-        @input="(content)=> content = content">
+        @onContent="onContent">
 
       </editor>
-      <p @click="submit">提交</p>
+      <p class="btn-sub clear" @click="clear">清 空</p>
+      <p class="btn-sub" @click="submit">提 交</p>
     </div>
 </template>
 
@@ -26,21 +30,74 @@ export default {
   },
   data: function () {
     return {
-      content: '我是富文本编辑器的内容',
+      content: '',
       // tinymce的配置信息 参考官方文档 https://www.tinymce.com/docs/configure/integration-and-setup/
       editorSetting: {
-        height: 400
+        height: 600
       }
     }
   },
   methods: {
     submit () {
-      console.log(this.$refs.edit)
+      console.log(this.content)
+    },
+    clear () {
+      this.$refs.edit.clear()
+    },
+    onContent (txt) {
+      this.content = txt
     }
   }
 }
 </script>
 
 <style scoped>
-
+  .title-box{
+    display: flex;
+    margin-bottom: 20px;
+    flex-direction: row;
+    align-items: center;
+  }
+  .title{
+    font-size: 20px;
+    font-weight: bold;
+  }
+  .input{
+    flex: 1;
+    margin-left: 12px;
+    border-radius: 4px;
+    line-height: 32px;
+    outline: none;
+    padding: 0px 10px;
+    border-style: solid;
+    border-width: 1px;
+    border-color: #a8a8a8;
+  }
+  .form-box{
+    padding: 60px;
+  }
+  .btn-sub{
+    display: inline-block;
+    font-size: 16px;
+    padding: 10px 30px;
+    background-color: #348eed;
+    border-radius: 4px;
+    cursor: pointer;
+    color: white;
+    margin-top: 20px;
+    transition: opacity .3s linear;
+    opacity: 1;
+  }
+  .btn-sub:hover{
+    opacity: .8;
+  }
+  .btn-sub:active{
+    position: relative;
+    left:1px;
+    top:1px;
+  }
+  .btn-sub.clear{
+    margin-right: 20px;
+    background-color: gainsboro;
+  }
 </style>
